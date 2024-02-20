ce9a14074dc19799e635b1f8704467a34e0492c9
Rodrigo Machado <r.drigomachado@gmail.com>
2024-02-20 11:48:52 -0300

    notebook separado

---
+++ gerencia-2/setup.sh
@@ -0,0 +1,39 @@
+#Inicia minikube
+minikube start --driver=docker --cpus 4 --memory 3926
+
+#Configuracao basica do istio
+cd ~Documents
+curl -L https://istio.io/downloadIstio | sh -
+cd istio-1.20.0
+export PATH=$PWD/bin:$PATH
+istioctl install --set profile=demo -y
+
+#habilita injecao de codigo istio por padrao nos pods que forem lancados no namespace default
+kubectl label namespace default istio-injection=enabled
+
+#Clona projeto de gerencia
+git clone git@github.com:RdrigoMachado/gerencia-rede.git
+
+#Faz o deploy dos apps v1 e v2 e seus respectivos servicos
+cd gerencia-rede
+kubectl apply -f gerencia-deploy-v1.yaml
+kubectl apply -f gerencia-deploy-v2.yaml
+kubectl apply -f gerencia-deploy-v3.yaml
+kubectl apply -f gerencia-admin-deploy.yaml
+kubectl apply -f gerencia-deploy-notebook.yaml
+
+#Cria rotas para cada um dos servicos nas urls ingress_ip/v1 e ingress_ip/v2
+kubectl apply -f gerencia-gateway.yaml
+
+#Pegar os links para ambos os servicos
+ingress_ip=$(kubectl get services -n istio-system istio-ingressgateway -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+echo "link v1: http://$ingress_ip/v1"
+echo "link v2: http://$ingress_ip/v2"
+echo "link v2: http://$ingress_ip/loadbalancer"
+echo "link v2: http://$ingress_ip/admin"
+
+#Acesso ao servico de admin necessita passar o header user-type como admin
+curl -X GET http://$ingress_ip/admin -H "user-type: admin" -H "user-name: Root"
+
+#Caso não tenha esse header ou o valor for diferente o acesso sera negado
+curl -X GET http://$ingress_ip/admin
